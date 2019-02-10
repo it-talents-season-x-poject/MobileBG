2b94822a225ffdba6265ca86e46e2b06ff3c44ab
Yanitsa <yani.valeva@gmail.com>
2019-02-10 02:08:14 +0200

    Update search component to change its elements when changing the category

--- controler/cars.js
+++ controler/cars.js
@@ -1,4 +1,62 @@
 $(function() {
+    const DEFAULT_OPTION = '<option value="all">всички</option>';
+    var startYear = 1930;
+    var endYear = new Date().getFullYear();
+    const TOWNS = ['Благоевград', 'Бургас', 'Варна', 'Велико Търново', 'Видин', 'Враца', 'Габрово', 'Добрич', 'Кърджали', 'Кюстендил', 'Ловеч', 'Монтана', 'Перник', 'Плевен', 'Пловдив', 'Русе', 'Силистра', 'Сливен', 'София'];
+
+    for (let num = 1; num <= 21; num++) {
+        $('#gear').append(`<option value="${num}">${num}</option>`);
+    }
+
+    $('#gear').append(`<option value="${'nospeeds'}">${'без скорости'}</option>`)
+
+    for (let i = 0; i < TOWNS.length; i++) {
+        $('#city').append(`<option value="${i+1}">${TOWNS[i]}</option>`);
+    }
+
+    for (let y = endYear; y >= startYear; y--) {
+        $('#year').append(`<option value="${y}">след ${y}</option>`);
+    }
+
+    for (const make in makes['cars']) {
+        $('#make').append($(`<option value="${make}">${makes['cars'][make]}</option>`));
+    }
+   
+    $('#category').on('change', function() {
+        var category = $('#category').val();
+        var makeSelect = $('#make');
+        $(makeSelect).html(DEFAULT_OPTION);
+        $('#search-title').text(categories[category]);
+        for (let make in makes[category]) {           
+            makeSelect.append($(`<option value="${make}">${makes[category][make]}</option>`));
+        }
+
+        $('.icon-container > i').attr('class', `${vehicleIcons[category]} icon`);
+
+        if (category === 'bicycles') {
+            $('.engine-container').css('display', 'none');
+            $('.city-container').css('display', 'flex');
+            $('.gearbox-container').css('display', 'none');
+            $('.gear-container').css('display', 'flex');
+        } else {
+            $('.engine-container').css('display', 'flex');
+            $('.city-container').css('display', 'none');
+            $('.gearbox-container').css('display', 'flex');
+            $('.gear-container').css('display', 'none');
+        }
+
+        $('.article-search select:not(#category)').val('all');
+    });
+
+    $('#make').on('change', function() {
+        var make = $('#make').val();
+        var modelSelect = $('#model');
+        $(modelSelect).html(DEFAULT_OPTION);
+        for (let model in models[make]) {           
+            modelSelect.append($(`<option value="${model}">${models[make][model]}</option>`));
+        }
+    });
+
     $('#overlay-close-btn').on('click', function() {
         $('#overlay').css('display', 'none');
     });
---
+++ model/availableCarsStorage.js
@@ -0,0 +1,119 @@
+let categories = {
+    cars: 'Автомобили и джипове',
+    trucks: 'Камиони',
+    motorcycles: 'Мотоциклети',
+    bicycles: 'Велосипеди'
+};
+
+let makes = {
+    cars: {
+        audi: 'Audi',
+        bmw: 'BMW',
+        citroen: 'Citroen',
+        ferrari: 'Ferrari',
+        hyundai: 'Hyundai',
+        jeep: 'Jeep',
+        vw: 'VW'
+    },
+    trucks: {
+        gaz: 'Gaz',
+        ifa: 'Ifa',
+        zil: 'Zil'
+    },
+    motorcycles: {
+        balkan: 'Balkan',
+        honda: 'Honda',
+        kawasaki: 'Kawasaki',
+        suzuki: 'Suzuki'
+    },
+    bicycles: {
+        bmx: 'BMX'
+    }
+}
+
+let models = {
+    audi: {
+        a1: 'A1',
+        a2: 'A2',
+        a5: 'A5',
+        a6: 'A6'
+    },
+    bmw: {
+        x3: 'X3',
+        x4: 'X4',
+        x5: 'X5',
+        x6: 'X6'
+    },
+    citroen: {
+        c3: 'C3',
+        c4: 'C4'
+    },
+    ferrari: {
+        california: 'California',
+        testarossa: 'Testarossa'
+    },
+    hyundai: {
+        accent: 'Accent',
+        genesis: 'Genesis'
+    },
+    jeep: {
+        cherokee: 'Cherokee',
+        renegade: 'Renegade'
+    },
+    vw: {
+        arteon: 'Arteon',
+        golfVariant: 'Golf Variant',
+        lupo: 'Lupo',
+        passat: 'Passat',
+        polo: 'Polo'
+    },
+    gaz: {
+       51: '51',
+       52: '52',
+       53: '53' 
+    },
+    ifa: {
+        l: 'L',
+        w: 'W'
+    },
+    zil: {
+        130: '130',
+        131: '131',
+        133: '133' 
+    },
+    balkan: {
+        250: '250',
+        50: '50'
+    },
+    honda: {
+        bigRed: 'Big Red',
+        camino: 'Camino',
+        integra: 'Integra',
+        tiger: 'Tiger',
+        zoomer: 'Zoomer'
+    },
+    kawasaki: {
+        mule: 'Mule',
+        ninja: 'Ninja'
+    },
+    suzuki: {
+        hokuto: 'Hokuto',
+        impulse: 'Impulse',
+        savage: 'Savage',
+        streetMagic: 'Street Magic',
+        tornado: 'Tornado'
+    },
+    bmx: {
+        balkan: 'Balkan',
+        barbie: 'Barbie',
+        focus: 'Focus',
+        giant: 'Giant'
+    }
+}
+
+let vehicleIcons = {
+    cars: 'fas fa-car-side',
+    trucks: 'fas fa-truck',
+    motorcycles: 'fas fa-motorcycle',
+    bicycles: 'fas fa-bicycle'
+}
