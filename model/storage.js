f85e14566a77dc665665da1097449f4631c03809
Yanitsa <yani.valeva@gmail.com>
2019-02-11 20:40:50 +0200

    Add default vechicle ads

--- model/storage.js
+++ model/storage.js
@@ -1,4 +1,4 @@
-var userStorage = (function() {
+var userStorage = (function () {
     var num = 1;
 
     class User {
@@ -17,14 +17,14 @@ var userStorage = (function() {
             if (currentUser) {
                 this.addUserToLocalSesstion(currentUser.id);
             }
-            
+
             return currentUser;
         },
 
         register: function (email, password) {
             let currentUser = new User(email, password);
             userList.push(currentUser);
-            localStorage.setItem('userList', JSON.stringify(userList));   
+            localStorage.setItem('userList', JSON.stringify(userList));
             this.addUserToLocalSesstion(currentUser.id);
         },
 
@@ -36,15 +36,15 @@ var userStorage = (function() {
 
 
 
-var vehicleStorage = (function(){
-    class Vehicle{
-        constructor(title, image, price, kilometres, city, manufacturedDate, engine, gearbox, color, moreInformation) {
+var vehicleStorage = (function () {
+    class Vehicle {
+        constructor(title, image, price, kilometres, city, manufacturedYear, engine, gearbox, color, moreInformation) {
             this.title = title;
             this.image = image;
             this.price = price;
             this.kilometres = kilometres;
             this.city = city;
-            this.manufacturedDate = manufacturedDate;
+            this.manufacturedYear = manufacturedYear;
             this.engine = engine;
             this.gearbox = gearbox;
             this.color = color;
@@ -53,18 +53,32 @@ var vehicleStorage = (function(){
         }
     }
 
-        var carList = [];
-        var motorcycleList = [];
-        var truckList = [];
-        var bicyckleList = [];
-    
-    return{
-        addVehicle: function(title,img,price,kilometres,city){
-            return 
-        }
-    }
-    
-    
+    function createDefaultVechicleAds() {
+        const vechicleItems = [
+            new Vehicle('Audi A5', '../assets/images/audi-a5.jpg', 20000, 140000, 'София', 2010, 'Дизелов', 'Автоматична', 'сив', ''),
+            new Vehicle('BMW X3', '../assets/images/bmw-x3.jpg', 25000, 100000, 'Пловдив', 2016, 'Бензинов', 'Автоматична', 'син', ''),
+            new Vehicle('Citroen C3', '../assets/images/citroen-c3.jpg', 27000, 140000, 'София', 2019, 'Дизелов', 'Ръчна', 'бял', ''),
+            new Vehicle('Ferrari Testarossa', '../assets/images/ferrari-testarossa.jpg', 17000, 240000, 'София', 2010, 'Бензинов', 'Ръчна', 'червен', ''),
+            new Vehicle('Hyundai Accent', '../assets/images/hyundai-accent.jpg', 30000, 50000, 'София', 2018, 'Дизелов', 'Автоматична', 'червен', ''),
+            new Vehicle('Volkswagen Polo', '../assets/images/volkswagen-polo.jpg', 16000, 10000, 'София', 2017, 'Бензинов', 'Ръчна', 'оранжев', '')
+        ]
 
+        localStorage.setItem('carList', JSON.stringify(vechicleItems));
+    }
+
+    if (!localStorage.getItem('carList')) {
+        createDefaultVechicleAds();
+    }
+
+    var carList = JSON.parse(localStorage.getItem('carList'));
+    var motorcycleList = [];
+    var truckList = [];
+    var bicyckleList = [];
+
+    return {
+        addVehicle: function (title, img, price, kilometres, city) {
+            return
+        },
+    }
 })();
 
